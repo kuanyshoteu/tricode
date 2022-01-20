8f0f1542a1fbaec3470a0404cdd04de29e0c97b1
Kuanysh <kuanyshoteu@gmail.com>
2022-01-21 01:30:08 +0600

    server

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-toserver = False
+toserver = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-gcz=%&z0&c476=^4srp1=&x(g%mx%ii1)!obhnpuga-)!vjxlx'
